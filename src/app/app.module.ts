843709c2fac7bb3e683530cf757ceb546be5f5bf
nhermann <nhermann@seyes.org>
2017-12-07 16:03:43 +0100

    + input vac

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing.modules'
 
@@ -9,6 +10,7 @@ import { Vac } from './vac/app.vac';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
 
 import { NgxGalleryModule } from 'ngx-gallery';
 
@@ -21,9 +23,11 @@ import { NgxGalleryModule } from 'ngx-gallery';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
     MatTabsModule,
+    MatInputModule,
     NgxGalleryModule
   ],
   providers: [],
